0521b8468688aa48e22a090a714b4169df6e112f
Pascal <pascalokereke18@gmail.com>
2023-10-10 20:44:53 +0100

    @bot.command

--- bott.py
+++ bott.py
@@ -6,32 +6,33 @@ import os
 
 # import load_dotenv function from dotenv module.
 from dotenv import load_dotenv
+
 # loads the dotenv file
+# import commands from the discord.ext module.
+from discord.ext import commands
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-Text = os.getenv("Text")
+intents = discord.intents.default()
 
-bot = discord.Client()
-
-# event listener for when the bot has switched from offline to online
-@bot.event 
-async def on_ready():
-    guild_count = 0
+# creates a new bot object with a specified prefix.
+bot = commands.Bot(command_prefix="$", intents=intents)
     
-# loop through all the server the bot is connected to 
-for guild in bot.guilds:
-    print(f"- {guild.id} (name: {guild.name})")
-    
-    guild_count = guild_count + 1
-    
-    print("Ai cub is in" + str(guild_count) + "guilds.")
-    
-# event listener for when a new message is sent to a channel.
+# on_message() event listener.
 @bot.event
 async def on_message(message):
     if message.content == "hello":
-        await message.channel.send("hi, welcome")
+        await message.channel.send("hi, welcome to the AI server")
         
+    await bot.process_commands(message)
+    
+@bot.command (name="ping")
+
+async def print(ctx, *args):
+    response = ""
+    
+    for arg in args:
+        response = response + " " + arg 
+        
+    await ctx.channel.send(response)
 bot.run(DISCORD_TOKEN)
-bot.run(Text)  
